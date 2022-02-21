8eb5b64da7359cac139ba662337a5ba304cf8093
Alexander Belchev <randomryze1@gmail.com>
2022-02-21 19:07:24 +0200

    IADS MergeSort Homework

---
+++ src/IADS_Work/MergeSort/MergeSortMain.java
@@ -0,0 +1,18 @@
+package IADS_Work.MergeSort;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MergeSortMain {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int[] array = rand.ints(input.nextInt(), 0, 100).toArray();
+
+        System.out.println("Before sort " + Arrays.toString(array));
+        MergeSortMethods.sort(array, 0, array.length - 1);
+        System.out.println("After sort " + Arrays.toString(array));
+
+    }
+}
