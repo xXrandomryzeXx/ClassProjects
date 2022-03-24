68a860e59a255a0e01ad793de07c63486688cb8f
Alexander Belchev <randomryze1@gmail.com>
2022-03-24 10:55:25 +0200

    ClassWork 11 -> Ex. 10 in Google Classroom

---
+++ src/ClassWork11/ArrayWork.java
@@ -0,0 +1,74 @@
+package ClassWork11;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ArrayWork {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+        String[] arr = text.split(" ");
+        int n = Integer.parseInt(input.nextLine());
+
+        for(int i = 0; i < n; i++){
+            String[] command = input.nextLine().split(" ");
+            if(command[0].equals("Reverse")){
+                arr = Reverse(arr);
+            }else if(command[0].equals("Distinct")){
+                arr = Distinct(arr);
+            }else if(command[0].equals("Replace")){
+                arr = Replace(arr, Integer.parseInt(command[1]), command[2]);
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + ((i < arr.length - 1)? ", ": ""));
+        }
+
+    }
+
+    public static String[] Reverse(String[] array){
+        String[] reversed = new String[array.length];
+        int j = 0;
+        for(int i = array.length-1; i >= 0; i--) {
+            reversed[j] = array[i];
+            j += 1;
+        }
+        return reversed;
+    }
+    // Leave only the distinct elements in the array (unique)
+    public static String[] Distinct(String[] array){
+        int distinctElements = array.length;
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                try{
+                    if(i != j && array[i].equals(array[j]) && array[i]!=null){
+                        array[j] = null;
+                        distinctElements -= 1;
+                    }
+                }catch(NullPointerException ignored){
+
+                }
+
+            }
+        }
+        String[] distinctArr = new String[distinctElements];
+        int k = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] != null){
+                distinctArr[k] = array[i];
+                k+=1;
+            }
+        }
+        return distinctArr;
+    }
+
+    public static String[] Replace(String[] array, int index, String element){
+        String[] replaced = Arrays.copyOf(array, array.length);
+        replaced[index] = element;
+        return replaced;
+    }
+
+
+}
