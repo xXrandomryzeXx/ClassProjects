8e20c87bcc8baf2886adc014ac8d25efab2a6504
Alexander Belchev <randomryze1@gmail.com>
2022-04-10 18:27:42 +0300

    Theme 17 in classroom IADS (УАСД)

---
+++ src/IADS_Work/SchoolGrades/Main.java
@@ -0,0 +1,115 @@
+package IADS_Work.SchoolGrades;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String command = "";
+
+        ArrayList<Student> studentsDiary = new ArrayList<>();
+        Student a = new Student("John", "Smith");
+        Student b = new Student("Smith", "Johnson");
+        studentsDiary.add(a);
+        studentsDiary.add(b);
+        addSubject(studentsDiary, "Math");
+        addSubject(studentsDiary, "English");
+        addSubject(studentsDiary, "Physics");
+        a.addGrade("Math", 5.5);
+        a.addGrade("English", 5);
+        a.addGrade("Physics", 6);
+
+        b.addGrade("Math", 6);
+        b.addGrade("English", 4);
+        b.addGrade("Physics", 5);
+
+
+        enum Menu{
+            MAIN,
+            EDIT,
+            ADD_STUDENT,
+            ADD_SUBJECT,
+            REMOVE_STUDENT,
+            REMOVE_SUBJECT,
+            DISPLAY_DIARY,
+            EXIT
+        }
+        Menu currentMenu = Menu.MAIN;
+
+        //Main menu
+        do {
+            switch(currentMenu){
+                case MAIN:
+                    System.out.println("1. Edit Diary");
+                    System.out.println("2. Display Diary");
+                    System.out.println("3. Exit");
+
+                    command = input.nextLine();
+
+                    switch(Integer.parseInt(command)){
+                        case 1:
+                            currentMenu = Menu.EDIT;
+                            break;
+                            case 2:
+                                currentMenu = Menu.DISPLAY_DIARY;
+                                break;
+                            case 3:
+                                currentMenu = Menu.EXIT;
+                                break;
+                    }
+
+                    break;
+
+                case EDIT:
+
+                    break;
+
+                case ADD_STUDENT:
+
+                    break;
+
+                case ADD_SUBJECT:
+
+                    break;
+
+                case REMOVE_STUDENT:
+
+                    break;
+
+                case REMOVE_SUBJECT:
+
+                    break;
+
+                case DISPLAY_DIARY:
+                    currentMenu = Menu.MAIN;
+                    break;
+            }
+        }while(currentMenu != Menu.EXIT);
+    }
+
+    // Student class has a method that will add a subject to the student, but not to everyone else
+    // hence why this method is needed, when making a diary.
+    // NOTE: Additional checks should be added if students are to be separated by grade
+    // Example: Mike->grade 1 has some classes different from John->grade 2 etc.
+    public static void addSubject(ArrayList<Student> diary, String subject){
+        for(Student student : diary){
+            student.addSubject(subject);
+        }
+    }
+    // Same principle as addSubject
+    public static void removeSubject(ArrayList<Student> diary, String subject){
+        for(Student student : diary){
+            student.removeSubject(subject);
+        }
+    }
+
+    public static String generateString(String str, int n){
+        String string = "";
+        for(int i = 0; i < n; i++){
+            string += str;
+        }
+        return string;
+    }
+}
---
+++ src/IADS_Work/SchoolGrades/Student.java
@@ -0,0 +1,80 @@
+package IADS_Work.SchoolGrades;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    private String first_name = "";
+    private String last_name = "";
+    private long student_id = 0;
+
+    // HashMap<Subject, Grades>
+    private HashMap<String, ArrayList<Double>> grades = new HashMap<>();
+
+    Student(String first_name, String last_name){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        student_id = generateId();
+    }
+
+    //Adds a subject to the grades list
+    public void addSubject(String subject){
+        if(!grades.containsKey(subject)){
+            grades.put(subject, new ArrayList<>());
+        }
+    }
+
+    // Adds a grade to a subject, if it's available
+    public void addGrade(String subject, double grade){
+        if(grades.containsKey(subject)){
+            grades.get(subject).add(grade);
+        }
+    }
+
+    // Removes a subject from the grades list
+    public void removeSubject(String subject){
+        grades.remove(subject);
+    }
+
+    // Removes a grade from the grades of a subject
+    public void removeGrade(String subject, double grade){
+        grades.get(subject).remove(grade);
+    }
+
+    // Returns all subjects that the student has grades for
+    public ArrayList<String> getSubjects(){
+        return new ArrayList<>(grades.keySet());
+    }
+    // Returns an list of all grades for the given subject
+
+    public ArrayList<Double> getGrades(String subject){
+        return grades.get(subject);
+    }
+
+    // Returns the first name of the student
+    public String getFirstName(){
+        return first_name;
+    }
+    // Returns the last name of the student
+    public String getLastName(){
+        return last_name;
+    }
+    // Returns the ID of the student
+    public long getStudentId(){
+        return student_id;
+    }
+
+    // Generates a custom ID for a student
+    // It could be made to generate one using date of creation of the student file
+    // and birthday
+    private long generateId(){
+        long id = 0;
+        for(char c : first_name.toCharArray()){
+            id += c + 1257;
+        }
+        for(char c : last_name.toCharArray()){
+            id += (long) c * c;
+        }
+        return id;
+    }
+}
